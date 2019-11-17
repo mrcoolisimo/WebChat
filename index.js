23107565d03e0ed84659d2faf4dbdc65fd96c315
MrCoolisimo <the5secondninja@gmail.com>
2019-11-16 23:00:16 -0800

    Chat system first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var socket = require('socket.io')
+
+// App Setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log("Listening on port 4000");
+});
+
+// Static Files
+app.use(express.static('public'));
+
+//Socket Setup
+var io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made socket connection',socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+});
