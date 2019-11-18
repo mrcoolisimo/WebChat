ce061ac0bec3ac4449019c55bdbde5e64c908de0
MrCoolisimo <the5secondninja@gmail.com>
2019-11-17 20:52:49 -0800

    first chat app

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+// Acquire Modules -- Express and Sockets
 var express = require('express');
 var socket = require('socket.io')
 
-// App Setup
+// App Setup -- Telling Express to listen on Port 4000
 var app = express();
 var server = app.listen(4000, function(){
     console.log("Listening on port 4000");
 });
 
-// Static Files
+// Static Files -- Looks for this folder to serve the website
 app.use(express.static('public'));
 
-//Socket Setup
+
+
+// Socket Setup -- Tell Socket.io to listen to this server
 var io = socket(server);
 
+// Handle Chat Event -- Listens for event called "connection"
 io.on('connection', (socket) => {
+    // Socket ID is a unique code for each connected user assigned upon connecting
     console.log('made socket connection',socket.id);
 
+    
     socket.on('chat', function(data){
         io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit("typing", data);
     });
 });
